db8ca60fded3c20a1db2618e488c0c594f39f209
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-08-14 13:09:23 +0900

    問題修正完了

--- components/layout/ProjectEditor.vue
+++ components/layout/ProjectEditor.vue
@@ -1,20 +1,21 @@
 <template lang="pug">
 .form-container(
-  :class="{ 'not-editable': !(props.editable || useRoute().query.admin) }"
+  :class="{ 'not-editable': !(props.editable || useRoute().query.admin) }",
+  ref="formContainer"
 )
   Head
     Title {{ data.title || "Editor" }}
   div
     label.form-label(for="input-title") タイトル
       button.btn.btn-sm.p-0.ms-2.bi-question-circle(
-        v-bTooltip.right.html,
+        data-bs-toggle="tooltip",
         title="文字数制限はない。<br>20-40文字程度を想定。"
       )
     input#input-title.form-control(v-model="data.title", placeholder="タイトル...")
   div
     label.form-label(for="input-description") 詳細
       button.btn.btn-sm.p-0.ms-2.bi-question-circle(
-        v-bTooltip.right.html,
+        data-bs-toggle="tooltip",
         title="Markdown記法が利用できる。"
       )
     PartsFlexMonospaceEditor#input-description.form-control(
@@ -24,7 +25,7 @@
   div
     label.form-label(for="input-tags") タグ(検索キーワードとして参照されます。)
       button.btn.btn-sm.p-0.ms-2.bi-question-circle(
-        v-bTooltip.right.html,
+        data-bs-toggle="tooltip",
         title="多めに書いておくことを推奨します。"
       )
     VSelect(
@@ -37,7 +38,7 @@
   div
     label.form-label(for="input-parent") 所属親
       button.btn.btn-sm.p-0.ms-2.bi-question-circle(
-        v-bTooltip.right.html,
+        data-bs-toggle="tooltip",
         title="所属の親を選択してください。<br>所属は作成することも可能です。"
       )
     VSelect(
@@ -49,7 +50,7 @@
   div
     label.form-label(for="input-type") コンテンツタイプ
       button.btn.btn-sm.p-0.ms-2.bi-question-circle(
-        v-bTooltip.right.html,
+        data-bs-toggle="tooltip",
         title="無し: コンテンツが以下の6つに当てはまらない場合。<br>Youtube: コンテンツがYoutube動画の場合。<br>GoogleDrive: コンテンツがGoogleDriveのファイル(PDFやDocument,Spreadsheetなど)の場合。"
       )
     select#input-type.form-control(v-model="data.type")
@@ -64,14 +65,14 @@
   div
     label.form-label(for="input-id") ファイルID
       button.btn.btn-sm.p-0.ms-2.bi-question-circle(
-        v-bTooltip.right.html,
+        data-bs-toggle="tooltip",
         title="URLではありません。ファイルのIDです。"
       )
     input#input-id.form-control(v-model="data.id", placeholder="ファイル、動画のID")
   div
     label.form-label(for="input-thumbnail") サムネイルURL
       button.btn.btn-sm.p-0.ms-2.bi-question-circle(
-        v-bTooltip.right.html,
+        data-bs-toggle="tooltip",
         title="ドライブのファイルを用いる際は、<u>必ず</u>「ファイルコピー君」で取得したURLを使用してください。"
       )
     .input-group
@@ -83,7 +84,7 @@
   div
     label.form-label(for="input-ratio") コンテンツの比率
       button.btn.btn-sm.p-0.ms-2.bi-question-circle(
-        v-bTooltip.right.html,
+        data-bs-toggle="tooltip",
         title="入力しなければ自動で比率が決定します。"
       )
     input#input-ratio.form-control(
@@ -114,7 +115,7 @@
 import { onBeforeRouteLeave } from "vue-router";
 import getYoutubeThumbnailUrl from "~~/composables/getYoutubeThumbnailUrl";
 import { SFProject } from "~~/composables/SFProject";
-import { VBTooltip } from "bootstrap-vue-3";
+import * as bootstrap from "bootstrap";
 const VSelect = useVSelect();
 const props = withDefaults(
   defineProps<{
@@ -212,6 +213,15 @@ const saveData = async () => {
 
   savingData.value = false;
 };
+const formContainer = ref<HTMLDivElement>();
+onMounted(() => {
+  var tooltipTriggerList = Array.prototype.slice.call(
+    formContainer.value.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl, { html: true });
+  });
+});
 </script>
 
 <style scoped lang="scss">
