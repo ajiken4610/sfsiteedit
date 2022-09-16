4ff91b2aaf5e89e9f791fd7cae16f241131e9eb1
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-09-16 09:32:00 +0900

    デバッグトークン削除

--- composables/firebase.ts
+++ composables/firebase.ts
@@ -25,7 +25,7 @@ export const useFirebaseApp = () => app;
 export const useAnalytics = () => analytics;
 
 //@ts-ignore
-self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+// self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
 
 // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
 // key is the counterpart to the secret key you set in the Firebase console.
